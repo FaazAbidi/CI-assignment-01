8f24272fa19fdc8df8b81452dff5a711acd25758
hasan <hasannaseem2@gmail.com>
2022-02-13 03:57:26 +0500

    mutation working partially correct GC

--- EA.py
+++ EA.py
@@ -5,7 +5,7 @@ from enum import Enum
 # Global Parameters
 POPULATION_SIZE = 50
 OFFSPRING_SIZE = 30  # offspring size must be a multiple of 2 (even)
-GENERATIONS = 10000
+GENERATIONS = 100
 MUTATION_RATE = 0.5
 ITERATIONS = 10
 
@@ -48,11 +48,15 @@ class EA:
             parents = SelectionFunctions.binary_tournament(self.population, self.fitness_scores, OFFSPRING_SIZE)
         
         for i in range(0,OFFSPRING_SIZE,2):
-            child1 = self.problem.crossover(parents[i], parents[i+1])
+            child1 = self.problem.crossover(parents[i],parents[i+1])
             child2 = self.problem.crossover(parents[i],parents[i+1])
+            # print('crossover', self.problem.check_chromosome(child1))
+            # print('crossover', self.problem.check_chromosome(child2))
 
             child1 = self.problem.mutation(child1, MUTATION_RATE)
             child2 = self.problem.mutation(child2, MUTATION_RATE)
+            print('mutation', self.problem.check_chromosome(child1), 'child1')        
+            print('mutation', self.problem.check_chromosome(child2), 'child2')
 
             self.population.append(child1)
             self.population.append(child2)
