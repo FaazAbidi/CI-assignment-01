8f336ec419af3d775c6d32ffc7204b8dcd679f23
Syed Hammad Ali <43202991+AliSyedHammad@users.noreply.github.com>
2022-02-13 19:22:52 +0500

    Knapsack 1019

--- Problems/knapsack_problem.py
+++ Problems/knapsack_problem.py
@@ -28,7 +28,7 @@ class KnapsackProblem(Problem):
                 self.weight_limit = int(line[1])
             else:
                 line = line.split()
-                self.data[int(line[0])] = int(line[1])
+                self.data[i-1] = (int(line[0]), int(line[1]))
         file.close()
 
     def __generate_initial_population(self)-> list:
@@ -38,15 +38,15 @@ class KnapsackProblem(Problem):
         """
         random_population = []
         for element in range(POPULATION_SIZE):
-            all_profits = list(self.data.keys())
-            random.shuffle(all_profits)
+            all_items = list(self.data.keys())
+            random.shuffle(all_items)
             # print(all_cities)
             knapsack = []
             current_weight = 0
             while(current_weight < self.weight_limit):
-                profit_i = all_profits.pop(0)
-                knapsack.append(profit_i)
-                current_weight += self.data[profit_i]
+                item = all_items.pop(0)
+                knapsack.append(item)
+                current_weight += self.data[item][1]
 
             random_population.append(knapsack)
         return random_population
@@ -67,7 +67,63 @@ class KnapsackProblem(Problem):
         This method will calculate the fitness of the chromosome
         """
         total_profit = 0
-        for profit in chromosome:
-            total_profit += profit
+        for item in chromosome:
+            total_profit += self.data[item][0]
         return total_profit
+
+    
+    def crossover(self, parent1: list, parent2: list) -> list:
+        """
+        This method will perform crossover on the parents and return the child. It selects a random range and selects a sublist from the parents
+        and merges them.
+        We have eliminated the possibility of wrong chromosomes. 
+        """
+        crossover_point1 = random.randint(0, len(parent1)-1)
+        crossover_point2 = random.randint(0, len(parent2)-1)
+        child = []
+        parent_flag = True
+        count = 0
+        while self.calculate_weight(child) < self.weight_limit and count < 100:
+            if parent_flag:
+                if not parent1[crossover_point1%len(parent1)] in child:
+                    child.append(parent1[crossover_point1%len(parent1)])
+                parent_flag = not parent_flag
+                crossover_point1+=1
+            else:
+                if not parent2[crossover_point2%len(parent2)] in child:
+                    child.append(parent2[crossover_point2%len(parent2)])
+                parent_flag = not parent_flag
+                crossover_point2+=1
+            count+=1
+        child.pop()
+        return child
+
+    def calculate_weight(self, chromosome: list) -> list :
+        """
+        This method will calculate the weight of the knapsack
+        """
+        total_weight = 0
+        for item in chromosome:
+            total_weight += self.data[item][1]
+        return total_weight
+
+
+    def mutation(self, child: list, mutation_rate: float()) -> list:
+        """
+        For the mutation, we will swap two values in the child at mutation_rate probability
+        """
+        random_val = random.random()
+        if random_val < mutation_rate:
+            index = random.randint(0, len(child)-1)
+            child.pop(index)
+            current_weight = self.calculate_weight(child)
+            new_index = random.randint(0, len(self.data)-1) 
+            # TODO: need to reduce while
+            count = 0
+            while (current_weight+self.data[new_index][1] > self.weight_limit or new_index in child) and count < 100: 
+                new_index = random.randint(0, len(self.data)-1)
+                count += 1
+            child.append(new_index)
+
+        return child
         
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from Problems.knapsack_problem import KnapsackProblem
 from Problems.graph_coloring_problem import GraphColoringProblem
 
 # instantiate the TSP problem
-TSP = TSPProblem()
+# TSP = TSPProblem()
 # applying the EA to the TSP problem
-EA = EA(TSP)
+# EA = EA(TSP)
 
-print(TSP.crossover(parent1=[1,3,4,5,6],parent2=[6,5,3,5,7]))
+# print(TSP.crossover(parent1=[1,3,4,5,6],parent2=[6,5,3,5,7]))
 
 tsp = TSPProblem()
 kp = KnapsackProblem()
@@ -18,9 +18,13 @@ ea = EA(kp)
 # print(kp.data)
 
 
-# # print(len(ea.population))
-# # print(ea.population)
-# print(SelectionFunctions.binary_tournament([1,2,3,4,5,6,7,8,9,10], [10,15,20,30,25,5,6,7,90,1000], 3,fit_bias=False))
+# print(len(ea.population))
+# print(ea.population)
+# print(SelectionFunctions.binary_tournament([1,2,3,4,5,6,7,8,9,10], [10,15,20,30,25,5,6,7,90,1000], 3))
+while ea.generation < GENERATIONS:
+    print(ea.generation, ea.worst_fitness_score())
+    ea.generate_offspring(Selection.ProportionalSelection)
+    ea.evaluate_population(Selection.Truncation)
 
 # print(ReproductiveFunctions.crossover(parent1=[1,2,7,3,4,5,8,9,6,10], parent2=[10,6,8,9,7,3,4,1,5,2]))
 # for u in range(10000):
